42af950a05cf548e49883ba5fe825c895ff3c7c3
E450 <yohoho233@gmail.com>
2018-07-19 13:18:39 +0800

    完善巨潮资讯网爬虫

--- cninfo.py
+++ cninfo.py
@@ -1,11 +1,10 @@
-from urllib import parse
 import requests
 import json
 import re
 import os
 
 
-def pdf_link(code):
+def pdf_links_get(code, years_limit):
     datas = {
         'stock': code,
         'category': "category_ndbg_szsh",
@@ -14,13 +13,10 @@ def pdf_link(code):
         'column': 'szse_sme',
         'tabName': 'fulltext'
     }
+    request_url = 'http://www.cninfo.com.cn/cninfo-new/announcement/query'
+    request_session = requests.session()
+    s = request_session.post(request_url, data=datas).text
 
-    Request_URL = 'http://www.cninfo.com.cn/cninfo-new/announcement/query'
-
-    requestSession = requests.session()
-    r = requestSession.post(Request_URL, data=datas)
-
-    s = r.text
     data = json.loads(s)['announcements']
     ret = []
     for item in data:
@@ -28,13 +24,30 @@ def pdf_link(code):
         if len(title) < 2:
             continue
         if len(title[1]) == 0:
-            tmp = {'year': title[0], 'link': item['adjunctUrl']}
-            ret.append(tmp.copy())
+            title_sp = re.findall("\d\d\d\d", title[0])
+            if len(title_sp) > 0 and title_sp[0] in years_limit:
+                tmp = {'year': title_sp[0], 'link': item['adjunctUrl']}
+                ret.append(tmp.copy())
     return ret
 
 
-def code_get():
-    Request_URL = 'http://www.cninfo.com.cn/cninfo-new/select/selectStock'
+def pdfs_links_get(codes_path, save_path, list_years):
+    year_limit = [str(i) for i in range(list_years[0], list_years[1] + 1)]
+    db_data = []
+    data = json.load(open(codes_path, 'r'))
+    count_num = 0
+    total_num = len(data)
+    for item in data:
+        count_num += 1
+        print("获取年报链接 共%s个公司 第%s个" % (total_num, count_num))
+        tmp = {'code': item['Col00']}
+        tmp['pdf'] = pdf_links_get(tmp['code'], year_limit)
+        db_data.append(tmp.copy())
+    json.dump(db_data, open(save_path, 'w'), ensure_ascii=False)
+
+
+def code_get(save_path):
+    request_url = 'http://www.cninfo.com.cn/cninfo-new/select/selectStock'
     params = {
         'area': '',
         'index': '',
@@ -42,111 +55,61 @@ def code_get():
         'market': '',
         'pagenum': 0
     }
-    requestSession = requests.session()
-    hasNextPage = True
+    request_session = requests.session()
+    has_next_page = True
     pagenum = 0
-    all_codes = []
-    while hasNextPage:
-        pagenum += 1
-        print('pagenum: %s' % pagenum)
-        params['pagenum'] = pagenum
-        text = requestSession.get(url=Request_URL, params=params).text
-        data = json.loads(text)
-        all_codes.append(data['items'])
-        hasNextPage = data['hasNextPage']
-    json.dump(all_codes, open('all_codes.json', 'w'), ensure_ascii=False)
-
-
-def data_json_create():
-    data = json.load(open('all_codes.json', 'r'))
+    page_total_num = "???"
     codes = []
-    for page in data:
-        codes += page
-    json.dump(codes, open('codes.json', 'w'), ensure_ascii=False)
-
-
-def pdf_link_save():
-    db_data = []
-    data = json.load(open('codes.json', 'r'))
-    count_num = 0
-    for item in data:
-        print(count_num)
-        count_num += 1
-        tmp = {}
-        tmp['code'] = item['Col00']
-        tmp['pdf'] = pdf_link(tmp['code'])
-        db_data.append(tmp.copy())
-    json.dump(db_data, open('pdf_link.json', 'w'), ensure_ascii=False)
-
-
-def tmp():
-    requestSession = requests.session()
-
-    Request_URL = "http://www.cninfo.com.cn/cninfo-new/disclosure/fund_listed_latest"
-    UA = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36'
-    requestSession.headers.update({'User-Agent': UA})
-    mainResponse = requestSession.get(Request_URL)
-    print(mainResponse.cookies.get_dict())
-
-    'sortName=&sortType=&limit=&showTitle=&exchange=&fundtype=&'
-
-    'stock=&searchkey=&plate=&category=&trade=&'
-    params = {
-        'column': 'fund_listed',
-        'columnTitle': '%E5%9F%BA%E9%87%91%E5%85%AC%E5%91%8A',
-        'pageNum': '2',
-        'pageSize': '30',
-        'tabName': 'latest',
-        'seDate': '%E8%AF%B7%E9%80%89%E6%8B%A9%E6%97%A5%E6%9C%9F'}
-    r = requestSession.post(Request_URL, data=params)
-    print(r.cookies.get_dict())
-    with open('debug.html', 'wb') as f:
-        s = r.text.encode('utf-8')
-        print(type(s))
-        f.write(s)
-
-    # print(parse.quote('上海'))
-    print(parse.unquote('%E8%AF%B7%E9%80%89%E6%8B%A9%E6%97%A5%E6%9C%9F'))
-
-
-def pdf_link_recode():
-    years = [str(i) for i in range(2000, 2019)]
-    datas = json.load(open('pdf_link.json', 'r'))
-    for i in range(len(datas)):
-        for j in range(len(datas[i]['pdf'])):
-            if datas[i]['pdf'][j]['year'] not in years:
-                tmp = re.findall("\d\d\d\d", datas[i]['pdf'][j]['year'])
-                if len(tmp) > 0:
-                    datas[i]['pdf'][j]['year'] = tmp[0]
-    json.dump(datas, open('pdf_link_recode.json', 'w'))
+    while has_next_page:
+        pagenum += 1
+        print('获取股票代码 共%s页 第%s页' % (page_total_num, pagenum))
+        params['pagenum'] = pagenum
+        text = request_session.get(url=request_url, params=params).text
+        data = json.loads(text)
+        codes += data['items']
+        has_next_page = data['hasNextPage']
+        page_total_num = data['totalPages']
+    json.dump(codes, open(save_path, 'w'), ensure_ascii=False)
 
 
 def file_download(url, filename):
     pdf = requests.get(url)
     pf = open(filename, 'wb')
-    pdf.raise_for_status()  # 函数查看下载文件是否出错，如果加载出错就抛出异常，否则就什么都不做。
+    pdf.raise_for_status()
     for buff in pdf.iter_content():
         pf.write(buff)
     pf.close()
 
 
-def down_load():
-    baseUrl = 'http://www.cninfo.com.cn/'
-    datas = json.load(open('pdf_link_recode.json', 'r'))
-    code_count = 0
+def down_load(link_path, save_path_base):
+    if not os.path.exists(save_path_base):
+        os.mkdir(save_path_base)
+    base_url = 'http://www.cninfo.com.cn/'
+    datas = json.load(open(link_path, 'r'))
+    pdf_count = 0
+    total_num = 0
     for data in datas:
-        code_count += 1
-        pdf_count = 0
+        total_num += len(data['pdf'])
+    for data in datas:
         code = data['code']
         for pdf in data['pdf']:
             pdf_count += 1
-            save_path = 'pdfs/' + code + '_' + pdf['year'] + '.pdf'
+            save_path = save_path_base + '/' + code + '_' + pdf['year'] + '.pdf'
             if os.path.exists(save_path):
                 continue
-            url = baseUrl + pdf['link']
+            print("下载年报 共%s份 第%s份" % (total_num, pdf_count))
+            url = base_url + pdf['link']
             file_download(url=url, filename=save_path)
-            print(code_count, pdf_count)
 
 
 if __name__ == '__main__':
-    down_load()
+    years = [2000, 2018]
+    codes_json = 'codes.json'
+    pdfs_links_json = 'pdfs_links.json'
+    pdf_save_path = 'pdfs'
+
+    if not os.path.isfile(codes_json):
+        code_get(codes_json)
+    if not os.path.isfile(pdfs_links_json):
+        pdfs_links_get(codes_json, pdfs_links_json, years)
+    down_load(pdfs_links_json, pdf_save_path)
